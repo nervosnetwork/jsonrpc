7c101f83a8fe34369c1b7a0e9b6721fcb0f91ee0
zhangsoledad <787953403@qq.com>
2019-02-12 14:53:03 +0800

    feat: rewrite http server

--- http/examples/server.rs
+++ http/examples/server.rs
@@ -1,19 +1,20 @@
-use jsonrpc_http_server::{ServerBuilder, DomainsValidation, AccessControlAllowOrigin, RestApi};
 use jsonrpc_http_server::jsonrpc_core::*;
+use jsonrpc_http_server::{AccessControlAllowOrigin, DomainsValidation, RestApi, ServerBuilder};
 
 fn main() {
-	let mut io = IoHandler::default();
-	io.add_method("say_hello", |_params: Params| {
-		Ok(Value::String("hello".to_string()))
-	});
+    let mut io = IoHandler::default();
+    io.add_method("say_hello", |_params: Params| {
+        Ok(Value::String("hello".to_string()))
+    });
 
-	let server = ServerBuilder::new(io)
-		.threads(3)
-		.rest_api(RestApi::Unsecure)
-		.cors(DomainsValidation::AllowOnly(vec![AccessControlAllowOrigin::Any]))
-		.start_http(&"127.0.0.1:3030".parse().unwrap())
-		.expect("Unable to start RPC server");
+    let server = ServerBuilder::new(io)
+        .threads(3)
+        .rest_api(RestApi::Unsecure)
+        .cors(DomainsValidation::AllowOnly(vec![
+            AccessControlAllowOrigin::Any,
+        ]))
+        .start_http(&"127.0.0.1:3030".parse().unwrap())
+        .expect("Unable to start RPC server");
 
-	server.wait();
+    server.wait();
 }
-
